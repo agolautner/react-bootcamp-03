d608e82ca76410cea78c4ba718a7d51b8497242e
agolautner <lautner.agoston@gmail.com>
2022-01-06 18:36:35 +0100

    19. slot machine

--- index.js
+++ index.js
@@ -2,24 +2,27 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <div>
-                    <Hello 
-                    to="Ringo"
-                    from="Paul"
-                    bangs={4}
-                    img="https://images.unsplash.com/photo-1641412287600-f99634339cce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80"
-                    // boolean={false}
-                    // booleanWithNoValueBecomesTrue
-                    // data={[1,2,3,4,5]}
-                    />
-                </div>
-                <div>
-                    <Hello 
-                    to="Britney"
-                    from="Adele"
-                    bangs={8}
-                    />
-                </div>
+                <h1>SLOT MACHINES</h1>
+                <Machine 
+                s1="🍊"
+                s2="🍇"
+                s3="🍎"
+                />
+                <Machine 
+                s1="🍊"
+                s2="🍇"
+                s3="🍎"
+                />
+                <Machine 
+                s1="🍇"
+                s2="🍇"
+                s3="🍇"
+                />
+                <Machine 
+                s1="🍊"
+                s2="🍎"
+                s3="🍊"
+                />
             </div>
         )
     }
